81872fa92a7334502aef1fee7e5e3eb2e4fc02c6
hiiamabat <hiiamabat@gmail.com>
2024-06-16 18:19:26 -0600

    Homepage initial final

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -46,7 +46,7 @@ export { Card };
 
 const AudioGear: React.FC = () => {
   return (
-    <section className="max-w-7xl 2xl:mx-auto bg-white pt-0 pb-20 px-7 2xl:p-0 2xl:flex 2xl:flex-row-reverse 2xl:my-16">
+    <section className="max-w-7xl 2xl:mx-auto bg-white pt-0 pb-20 px-7 xl:m-auto 2xl:p-0 2xl:flex 2xl:flex-row-reverse 2xl:my-16">
       <div className="2xl:w-1/2 2xl:flex">
         <picture className="w-auto rounded-default mb-10 mx-auto 2xl:ml-auto 2xl:mr-0">
           <source media="(min-width: 1024px)" srcSet={audioGearImageDesktop} />
@@ -58,11 +58,11 @@ const AudioGear: React.FC = () => {
           />
         </picture>
       </div>
-      <div className="2xl:w-1/2 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-start 2xl:text-left">
-        <h3 className="text-3xl text-secondary-black font-semibold tracking-wide pb-4">
+      <div className="2xl:w-1/2 2xl:flex 2xl:flex-col 2xl:justify-center 2xl:items-start 2xl:text-left 2xl:px-10">
+        <h3 className="text-3xl text-secondary-black font-semibold tracking-wide pb-4 xl:text-5xl 2xl:w-full">
           BRINGING YOU THE <span className="text-primary">BEST</span> AUDIO GEAR
         </h3>
-        <p className="">
+        <p className="text-md xl:text-lg xl:w-3/4 xl:m-auto 2xl:m-0 2xl:w-full">
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
@@ -89,7 +89,7 @@ const Footer: React.FC = () => {
             className="w-32 h-16 pt-6 m-auto md:m-0"
           />
         </Link>
-        <ul className="text-white text-xs tracking-widest font-bold pt-6 md:flex md:gap-6">
+        <ul className="text-white text-xs tracking-widest font-bold pt-6 md:flex md:gap-6 xl:text-sm">
           <li className="py-3">
             <Link className="hover:text-primary" to="/">
               HOME
@@ -111,7 +111,7 @@ const Footer: React.FC = () => {
             </Link>
           </li>
         </ul>
-        <p className="text-secondary-darkest pb-6">
+        <p className="text-secondary-darkest pb-6 xl:w-3/4 xl:text-lg">
           Audiophile is an all in one stop to fulfill your audio needs. We're a
           small team of music lovers and sound specialists who are devoted to
           helping you get the most out of personal audio. Come and visit our
@@ -120,19 +120,46 @@ const Footer: React.FC = () => {
         <div className="md:flex md:justify-between">
           <p className="pb-6">Copyright 2021. All Rights Reserved</p>
           <ul className="flex gap-4 items-center justify-center md:items-start pb-6">
-            <li>
-              <Link className="hover:fill-primary" to="/">
-                <img src={fbIcon} alt="Facebook Icon" />
+            <li className="relative">
+              <Link to="/">
+                <img
+                  src={fbIcon}
+                  alt="Facebook Icon"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-100 hover:opacity-0"
+                />
+                <img
+                  src={fbIconHover}
+                  alt="Facebook Icon Hover"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-0 hover:opacity-100 absolute top-0 left-0"
+                />
               </Link>
             </li>
-            <li>
-              <Link className="hover:fill-primary" to="/">
-                <img src={twitterIcon} alt="Twitter Icon" />
+            <li className="relative">
+              <Link to="/">
+                <img
+                  src={twitterIcon}
+                  alt="Twitter Icon"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-100 hover:opacity-0"
+                />
+                <img
+                  src={twitterIconHover}
+                  alt="Twitter Icon Hover"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-0 hover:opacity-100 absolute top-0 left-0"
+                />
               </Link>
             </li>
-            <li>
-              <Link className="hover:fill-primary" to="/">
-                <img src={instaIcon} alt="Instagram Icon" />
+            <li className="relative">
+              <Link to="/">
+                <img
+                  src={instaIcon}
+                  alt="Instagram Icon"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-100 hover:opacity-0"
+                />
+                <img
+                  src={instaIconHover}
+                  alt="Instagram Icon Hover"
+                  className="h-6 w-6 transition-opacity duration-300 opacity-0 hover:opacity-100 absolute top-0 left-0"
+                />
               </Link>
             </li>
           </ul>
@@ -157,7 +184,7 @@ const Home: React.FC = () => {
             <h1 className="text-4xl md:text-6xl font-semibold mb-4 uppercase sm:text-7xl xl:text-left">
               XX99 Mark II Headphones
             </h1>
-            <p className="text-lg w-4/5 m-auto mb-8 text-secondary-medium sm:w-2/3 sm:text-xl md:m-0 md:mb-8 xl:text-left">
+            <p className="text-lg w-4/5 m-auto mb-8 text-secondary-light sm:w-2/3 sm:text-xl md:m-0 md:mb-8 xl:text-left">
               Experience natural, lifelike audio and exceptional build quality
               made for the passionate music enthusiast.
             </p>
@@ -182,30 +209,34 @@ const Home: React.FC = () => {
         <Card imageSrc={earphonesImage} title="EARPHONES" linkTo="/earphones" />
       </section>
       {/* ZX9 SPEAKER SECTION */}
-      <section className="max-w-7xl rounded-default bg-primary bg-cover bg-speaker-bg-mobile text-white py-20 mx-7 w-auto md:bg-speaker-bg-tablet xl:mx-auto">
-        <img
-          src={speakerImage}
-          alt="ZX9 Speaker"
-          className="object-cover -mt-10 w-4/5 m-auto md:w-3/5 md:-mt-20"
-        />
-        <h3 className="text-3xl text-white font-semibold tracking-widest pb-4 uppercase md:text-6xl md:w-1/2 md:m-auto md:mt-10 md:pb-0">
-          ZX9 Speaker
-        </h3>
-        <p className="text-md w-4/5 m-auto text-white mb-8 md:w-1/2 md:my-8">
-          Upgrade to premium speakers that are phenomenally built to deliver
-          truly remarkable sound.
-        </p>
-        <div className="flex justify-center">
-          <Link
-            to="/speakers"
-            className="bg-secondary-black text-white text-sm font-normal tracking-widest hover:bg-primary transition duration-300 ease-in-out py-4 px-7"
-          >
-            SEE PRODUCT
-          </Link>
+      <section className="max-w-7xl rounded-default bg-primary bg-cover bg-speaker-bg-mobile text-white py-20 mx-7 w-auto md:bg-speaker-bg-tablet xl:mx-auto xl:mx-6 2xl:flex 2xl:justify-center 2xl:items-center 2xl:bg-speaker-bg-desktop 2xl:bg-bottom 2xl:p-0">
+        <div className="2xl:w-3/5 2xl:flex 2xl:flex-col 2xl:items-end 2xl:relative 2xl:overflow-hidden">
+          <img
+            src={speakerImage}
+            alt="ZX9 Speaker"
+            className="object-cover -mt-10 w-4/5 m-auto md:w-3/5 md:-mt-20 2xl:w-4/5 2xl:mt-0 2xl:mt-auto 2xl:-mb-[6rem] 2xl:pt-[5.5rem]"
+          />
+        </div>
+        <div className="2xl:w-2/5  2xl:justify-start 2xl:text-left 2xl:justify-self-start">
+          <h3 className="text-3xl text-white font-semibold tracking-widest pb-4 uppercase md:text-6xl md:w-1/2 md:m-auto md:mt-10 md:pb-0 2xl:m-0 2xl:mb-6">
+            ZX9 Speaker
+          </h3>
+          <p className="text-lg w-4/5 m-auto text-white mb-8 md:w-1/2 md:my-8 lg:text-xl 2xl:m-0 2xl:mb-8">
+            Upgrade to premium speakers that are phenomenally built to deliver
+            truly remarkable sound.
+          </p>
+          <div className="flex justify-center 2xl:justify-start">
+            <Link
+              to="/speakers"
+              className="bg-secondary-black text-white text-sm font-normal tracking-widest hover:bg-secondary-medium transition duration-300 ease-in-out py-4 px-7"
+            >
+              SEE PRODUCT
+            </Link>
+          </div>
         </div>
       </section>
       {/* ZX7 SPEAKER SECTION */}
-      <section className="max-w-7xl bg-speaker-pic-mobile bg-cover bg-right-top xs:bg-speaker-pic-tablet xs:bg-cover xs:bg-center flex rounded-lg w-auto m-auto mx-7 mt-8 pb-20 px-6 h-96 relative xl:mx-auto">
+      <section className="max-w-7xl bg-speaker-pic-mobile bg-cover bg-right-top xs:bg-speaker-pic-tablet xs:bg-cover xs:bg-center flex rounded-lg w-auto m-auto mx-7 mt-8 pb-20 px-6 h-96 relative xl:mx-auto xl:mx-6">
         <div className="absolute top-[30%] flex justify-start items-start flex-col 2xl:p-20 2xl:top-14">
           <h3 className="text-2xl text-black font-bold tracking-widest pb-4 uppercase justify-flex-start md:text-3xl md:mb-6">
             ZX7 SPEAKER
@@ -219,9 +250,9 @@ const Home: React.FC = () => {
         </div>
       </section>
       {/* YX1 EARPHONES SECTION */}
-      <section className="max-w-7xl md:flex xl:mx-auto">
-        <div className="bg-earphone-mobile bg-cover bg-center flex rounded-default m-auto w-auto mt-8 pb-20 px-4 h-60 mx-7 md:bg-earphone-tablet md:w-full md:ml-7 md:mr-2 md:h-[22rem] 2xl:mx-0 2xl:mr-8"></div>
-        <div className="bg-secondary-darker flex rounded-default m-auto w-auto mt-8 mb-20 pb-20 px-4 h-60 mx-7 relative md:w-full md:mr-7 md:ml-2 md:h-[22rem] 2xl:mx-0 2xl:p-8">
+      <section className="max-w-7xl md:flex xl:justify-center xl:items-center xl:mx-auto xl:mx-6">
+        <div className="bg-earphone-mobile bg-cover bg-center flex rounded-default m-auto w-auto mt-8 pb-20 px-4 h-60 mx-7 md:bg-earphone-tablet md:w-full md:ml-7 md:mr-2 md:h-[22rem] xl:mx-1 2xl:mx-0 2xl:mr-8"></div>
+        <div className="bg-secondary-darker flex rounded-default m-auto w-auto mt-8 mb-20 pb-20 px-4 h-60 mx-7 relative md:w-full md:mr-7 md:ml-2 md:h-[22rem] xl:mx-auto xl:ml-6 2xl:mx-0 2xl:p-8">
           <div className="absolute top-[30%] flex justify-start items-start flex-col content-center">
             <h3 className="text-2xl text-black font-bold tracking-widest pb-4 uppercase md:mb-6">
               YX1 EARPHONES
